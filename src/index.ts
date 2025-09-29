9ad3786ff33d05caa905866ec671f1b3bc50ed19
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-09-30 01:29:22 +0600

    add more tests and refactor some code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 "use strict";
 
-import { bool } from "./lib/bool";
-import { boolean } from "./lib/boolean";
-import { choice } from "./lib/choice";
-import { float } from "./lib/float";
-import { hex } from "./lib/hex";
-import { ifloat } from "./lib/ifloat";
-import { percentage } from "./lib/percentage";
-import { pick } from "./lib/pick";
-import { probability } from "./lib/probability";
-import { randInt } from "./lib/randInt";
-import { random } from "./lib/random";
-import { range } from "./lib/range";
-import { rangeInt } from "./lib/rangeInt";
-import { safeInt } from "./lib/safeInt";
-import { shuffle } from "./lib/shuffle";
-import { uSafeInt } from "./lib/uSafeInt";
+import { bool } from "@lib/bool";
+import { boolean } from "@lib/boolean";
+import { choice } from "@lib/choice";
+import { float } from "@lib/float";
+import { hex } from "@lib/hex";
+import { ifloat } from "@lib/ifloat";
+import { percentage } from "@lib/percentage";
+import { pick } from "@lib/pick";
+import { probability } from "@lib/probability";
+import { randInt } from "@lib/randInt";
+import { random } from "@lib/random";
+import { range } from "@lib/range";
+import { rangeInt } from "@lib/rangeInt";
+import { safeInt } from "@lib/safeInt";
+import { shuffle } from "@lib/shuffle";
+import { uSafeInt } from "@lib/uSafeInt";
 
 export {
   bool,
--- tests/inclusion.test.ts
+++ tests/inclusion.test.ts
@@ -1,10 +1,14 @@
+import index, * as exported from "@index";
 import { expect, test } from "bun:test";
-import index from "../src/index";
 
 test("check if everything is exported properly", () => {
   const scan = new Bun.Glob("src/lib/*.ts").scanSync();
   const methodNames = Array.from(scan).map(
     (fileName) => fileName.match(/[^\\/]+(?=\.ts$)/)![0],
   );
+  expect(exported).toMatchObject(index);
+  //@ts-expect-error
   expect(index).toContainKeys(methodNames);
+  //@ts-expect-error
+  expect(exported).toContainKeys(methodNames);
 });
